dec81e8ed8d892ab59f31af6d00ee48032aed117
Daniel Rankov <daniel@severalclouds.com>
2023-01-06 14:55:29 +0200

    hardening

--- README.md
+++ README.md
@@ -1,58 +1,27 @@
 
-# Welcome to your CDK Python project!
+# Welcome AWS SecurityHub CIS mitigation
 
-This is a blank project for CDK development with Python.
+## checklist
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
-
-This project is set up like a standard Python project.  The initialization
-process also creates a virtualenv within this project, stored under the `.venv`
-directory.  To create the virtualenv it assumes that there is a `python3`
-(or `python` for Windows) executable in your path with access to the `venv`
-package. If for any reason the automatic creation of the virtualenv fails,
-you can create the virtualenv manually.
-
-To manually create a virtualenv on MacOS and Linux:
+[] Modify the cdk bootstrap CF, to achieve compliance [S3.13] 
 
 ```
-$ python3 -m venv .venv
+cdk bootstrap --show-template > bootstrap-template.yaml
+```
+```
+      LifecycleConfiguration:
+        Rules:
+          - AbortIncompleteMultipartUpload:
+              DaysAfterInitiation: 1
+            Status: Enabled
+```
+```
+aws cloudformation update-stack --stack-name CDKToolkit --template-body file://bootstrap-template.yaml --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation create-change-set --stack-name CDKToolkit --template-body file://bootstrap-template.yaml --change-set-name CDKToolkit1 --capabilities CAPABILITY_NAMED_IAM
 ```
 
-After the init process completes and the virtualenv is created, you can use the following
-step to activate your virtualenv.
-
+[] ECR repositories should have at least one lifecycle policy configured [ECR.3]
 ```
-$ source .venv/bin/activate
+      LifecyclePolicy:
+        LifecyclePolicyText: '{"rules":[{"rulePriority":1,"selection":{"tagStatus":"untagged","countType":"imageCountMoreThan","countNumber":10000},"action":{"type":"expire"}}]}'
 ```
-
-If you are a Windows platform, you would activate the virtualenv like this:
-
-```
-% .venv\Scripts\activate.bat
-```
-
-Once the virtualenv is activated, you can install the required dependencies.
-
-```
-$ pip install -r requirements.txt
-```
-
-At this point you can now synthesize the CloudFormation template for this code.
-
-```
-$ cdk synth
-```
-
-To add additional dependencies, for example other CDK libraries, just add
-them to your `setup.py` file and rerun the `pip install -r requirements.txt`
-command.
-
-## Useful commands
-
- * `cdk ls`          list all stacks in the app
- * `cdk synth`       emits the synthesized CloudFormation template
- * `cdk deploy`      deploy this stack to your default AWS account/region
- * `cdk diff`        compare deployed stack with current state
- * `cdk docs`        open CDK documentation
-
-Enjoy!
--- aws_cdk2_cis/aws_cdk2_cis_stack.py
+++ aws_cdk2_cis/aws_cdk2_cis_stack.py
@@ -1,19 +1,292 @@
 from aws_cdk import (
-    # Duration,
+    CustomResource,
+    RemovalPolicy,
+    Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_s3 as s3,
+    aws_s3_notifications as s3n,
+    aws_cloudtrail as cloudtrail,
+    aws_kms as kms,
+    aws_logs as logs,
+    aws_iam as iam,
+    aws_config as config,
+    aws_sns as sns,
+    aws_cloudwatch_actions as cloudwatch_actions,
+    aws_cloudwatch as cloudwatch,
+    aws_lambda as _lambda,
+    custom_resources as cr,
+    aws_guardduty as guardduty,
+    aws_ecr as ecr,
 )
 from constructs import Construct
+from os import path
+
 
 class AwsCdk2CisStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        removal = RemovalPolicy.DESTROY
+        security_distribution_list_email = 'daniel+awssecurity@severalclouds.com'
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsCdk2CisQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        security_notifications_topic = sns.Topic(self, 'CIS_Topic',
+                                                 display_name='CIS_Topic',
+                                                 topic_name='CIS_Topic',
+                                                 )
+
+        sns.Subscription(self, 'CIS_Subscription',
+                         topic=security_notifications_topic,
+                         protocol=sns.SubscriptionProtocol.EMAIL,
+                         endpoint=security_distribution_list_email
+                         )
+
+        lifecycle_rule = s3.LifecycleRule(
+            abort_incomplete_multipart_upload_after=Duration.days(1),
+        )
+
+        # Ensure AWS Config is enabled / Ensure CloudTrail is enabled in all Regions 2.1 - 2.8
+        cloudtrail_bucket_accesslogs = s3.Bucket(self, "CloudTrailS3Accesslogs",
+                                                 block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+                                                 encryption=s3.BucketEncryption.S3_MANAGED,
+                                                 removal_policy=removal,
+                                                 enforce_ssl=True,
+                                                 lifecycle_rules=[
+                                                     lifecycle_rule]
+                                                 )
+
+        cloudtrail_bucket_accesslogs.add_event_notification(
+            s3.EventType.OBJECT_REMOVED,
+            s3n.SnsDestination(security_notifications_topic)
+        )
+
+        cloudtrail_bucket = s3.Bucket(self, "CloudTrailS3",
+                                      block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+                                      encryption=s3.BucketEncryption.S3_MANAGED,
+                                      removal_policy=removal,
+                                      server_access_logs_bucket=cloudtrail_bucket_accesslogs,
+                                      enforce_ssl=True,
+                                      lifecycle_rules=[
+                                          lifecycle_rule],
+                                      #   intelligent_tiering_configurations=[s3.IntelligentTieringConfiguration(
+                                      #       name="CloudTrailS3IntelligentTiering"
+                                      #   )]
+                                      )
+
+        cloudtrail_bucket.add_event_notification(
+            s3.EventType.OBJECT_REMOVED,
+            s3n.SnsDestination(security_notifications_topic)
+        )
+
+        cloudtrail_kms = kms.Key(self, "CloudTrailKey",
+                                 enable_key_rotation=True,
+                                 removal_policy=removal
+                                 )
+
+        cloudtrail_kms.grant(iam.ServicePrincipal(
+            'cloudtrail.amazonaws.com'), 'kms:DescribeKey')
+
+        cloudtrail_kms.grant(iam.ServicePrincipal(
+            'cloudtrail.amazonaws.com', conditions={
+                'StringLike': {'kms:EncryptionContext:aws:cloudtrail:arn': 'arn:aws:cloudtrail:*:'+Stack.of(self).account+':trail/*'}
+            }), 'kms:GenerateDataKey*')
+
+        cloudtrail_kms.add_to_resource_policy(iam.PolicyStatement(
+            actions=["kms:Decrypt", "kms:ReEncryptFrom"],
+            conditions={
+                'StringEquals': {'kms:CallerAccount': Stack.of(self).account},
+                'StringLike': {'kms:EncryptionContext:aws:cloudtrail:arn': 'arn:aws:cloudtrail:*:'+Stack.of(self).account+':trail/*'}
+            },
+            effect=iam.Effect.ALLOW,
+            principals=[iam.AnyPrincipal()],
+            resources=['*']
+        ))
+
+        cloudtrail_kms.add_to_resource_policy(iam.PolicyStatement(
+            actions=["kms:CreateAlias"],
+            conditions={
+                'StringEquals': {'kms:CallerAccount': Stack.of(self).account,
+                                 'kms:ViaService': 'ec2.' +
+                                 Stack.of(self).region+'.amazonaws.com'}
+            },
+            effect=iam.Effect.ALLOW,
+            principals=[iam.AnyPrincipal()],
+            resources=['*']
+        ))
+
+        cloudtrail_kms.add_to_resource_policy(iam.PolicyStatement(
+            actions=["kms:Decrypt", "kms:ReEncryptFrom"],
+            conditions={
+                'StringEquals': {'kms:CallerAccount': Stack.of(self).account},
+                'StringLike': {'kms:EncryptionContext:aws:cloudtrail:arn': 'arn:aws:cloudtrail:*:'+Stack.of(self).account+':trail/*'}
+            },
+            effect=iam.Effect.ALLOW,
+            principals=[iam.AnyPrincipal()],
+            resources=['*']
+        ))
+
+        # CloudTrail - single account, not Organization
+        trail = cloudtrail.Trail(self, "CloudTrail",
+                                 enable_file_validation=True,
+                                 is_multi_region_trail=True,
+                                 include_global_service_events=True,
+                                 send_to_cloud_watch_logs=True,
+                                 cloud_watch_logs_retention=logs.RetentionDays.FOUR_MONTHS,
+                                 bucket=cloudtrail_bucket,
+                                 encryption_key=cloudtrail_kms
+                                 )
+
+        config_role = iam.CfnServiceLinkedRole(self,
+                                               id='ServiceLinkedRoleConfig',
+                                               aws_service_name='config.amazonaws.com'
+                                               )
+
+        # global_config = config.CfnConfigurationRecorder(self, 'ConfigRecorder',
+        #                                                 name='default',
+        #                                                 role_arn="arn:aws:iam::"+Stack.of(self).account+":role/aws-service-role/config.amazonaws.com/AWSServiceRoleForConfig",
+        #                                                 recording_group=config.CfnConfigurationRecorder.RecordingGroupProperty(
+        #                                                     all_supported=True,
+        #                                                     include_global_resource_types=True
+        #                                                 )
+        #                                                 )
+
+        cloudwatch_actions_cis = cloudwatch_actions.SnsAction(
+            security_notifications_topic)
+
+        cis_metricfilter_alarms = {
+            'CIS-3.1-UnauthorizedAPICalls': '($.errorCode="*UnauthorizedOperation") || ($.errorCode="AccessDenied*")',
+            'CIS-3.2-ConsoleSigninWithoutMFA': '($.eventName="ConsoleLogin") && ($.additionalEventData.MFAUsed !="Yes")',
+            'RootAccountUsageAlarm': '$.userIdentity.type="Root" && $.userIdentity.invokedBy NOT EXISTS && $.eventType !="AwsServiceEvent"',
+            'CIS-3.4-IAMPolicyChanges': '($.eventName=DeleteGroupPolicy) || ($.eventName=DeleteRolePolicy) || ($.eventName=DeleteUserPolicy) || ($.eventName=PutGroupPolicy) || ($.eventName=PutRolePolicy) || ($.eventName=PutUserPolicy) || ($.eventName=CreatePolicy) || ($.eventName=DeletePolicy) || ($.eventName=CreatePolicyVersion) || ($.eventName=DeletePolicyVersion) || ($.eventName=AttachRolePolicy) || ($.eventName=DetachRolePolicy) || ($.eventName=AttachUserPolicy) || ($.eventName=DetachUserPolicy) || ($.eventName=AttachGroupPolicy) || ($.eventName=DetachGroupPolicy)',
+            'CIS-3.5-CloudTrailChanges': '($.eventName=CreateTrail) || ($.eventName=UpdateTrail) || ($.eventName=DeleteTrail) || ($.eventName=StartLogging) || ($.eventName=StopLogging)',
+            'CIS-3.6-ConsoleAuthenticationFailure': '($.eventName=ConsoleLogin) && ($.errorMessage="Failed authentication")',
+            'CIS-3.7-DisableOrDeleteCMK': '($.eventSource=kms.amazonaws.com) && (($.eventName=DisableKey) || ($.eventName=ScheduleKeyDeletion))',
+            'CIS-3.8-S3BucketPolicyChanges': '($.eventSource=s3.amazonaws.com) && (($.eventName=PutBucketAcl) || ($.eventName=PutBucketPolicy) || ($.eventName=PutBucketCors) || ($.eventName=PutBucketLifecycle) || ($.eventName=PutBucketReplication) || ($.eventName=DeleteBucketPolicy) || ($.eventName=DeleteBucketCors) || ($.eventName=DeleteBucketLifecycle) || ($.eventName=DeleteBucketReplication))',
+            'CIS-3.9-AWSConfigChanges': '($.eventSource=config.amazonaws.com) && (($.eventName=StopConfigurationRecorder) || ($.eventName=DeleteDeliveryChannel) || ($.eventName=PutDeliveryChannel) || ($.eventName=PutConfigurationRecorder))',
+            'CIS-3.10-SecurityGroupChanges': '($.eventName=AuthorizeSecurityGroupIngress) || ($.eventName=AuthorizeSecurityGroupEgress) || ($.eventName=RevokeSecurityGroupIngress) || ($.eventName=RevokeSecurityGroupEgress) || ($.eventName=CreateSecurityGroup) || ($.eventName=DeleteSecurityGroup)',
+            'CIS-3.11-NetworkACLChanges': '($.eventName=CreateNetworkAcl) || ($.eventName=CreateNetworkAclEntry) || ($.eventName=DeleteNetworkAcl) || ($.eventName=DeleteNetworkAclEntry) || ($.eventName=ReplaceNetworkAclEntry) || ($.eventName=ReplaceNetworkAclAssociation)',
+            'CIS-3.12-NetworkGatewayChanges': '($.eventName=CreateCustomerGateway) || ($.eventName=DeleteCustomerGateway) || ($.eventName=AttachInternetGateway) || ($.eventName=CreateInternetGateway) || ($.eventName=DeleteInternetGateway) || ($.eventName=DetachInternetGateway)',
+            'CIS-3.13-RouteTableChanges': '($.eventName=CreateRoute) || ($.eventName=CreateRouteTable) || ($.eventName=ReplaceRoute) || ($.eventName=ReplaceRouteTableAssociation) || ($.eventName=DeleteRouteTable) || ($.eventName=DeleteRoute) || ($.eventName=DisassociateRouteTable)',
+            'CIS-3.14-VPCChanges': '($.eventName=CreateVpc) || ($.eventName=DeleteVpc) || ($.eventName=ModifyVpcAttribute) || ($.eventName=AcceptVpcPeeringConnection) || ($.eventName=CreateVpcPeeringConnection) || ($.eventName=DeleteVpcPeeringConnection) || ($.eventName=RejectVpcPeeringConnection) || ($.eventName=AttachClassicLinkVpc) || ($.eventName=DetachClassicLinkVpc) || ($.eventName=DisableVpcClassicLink) || ($.eventName=EnableVpcClassicLink)',
+        }
+        for x, y in cis_metricfilter_alarms.items():
+            str_x = str(x)
+            str_y = str(y)
+            logs.MetricFilter(self, "MetricFilter_"+str_x,
+                              log_group=trail.log_group,
+                              filter_pattern=logs.JsonPattern(
+                                  json_pattern_string=str_y),
+                              metric_name=str_x,
+                              metric_namespace="LogMetrics",
+                              metric_value='1'
+                              )
+            cloudwatch.Alarm(self, "Alarm_"+str_x,
+                             alarm_name=str_x,
+                             alarm_description=str_x,
+                             comparison_operator=cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+                             evaluation_periods=1,
+                             threshold=1,
+                             metric=cloudwatch.Metric(metric_name=str_x,
+                                                      namespace="LogMetrics"
+                                                      ),
+                             ).add_alarm_action(cloudwatch_actions_cis)
+
+        # TODO KMS key for the SNS topic
+
+        support_role = iam.Role(self, "SupportRole",
+                                assumed_by=iam.AccountPrincipal(
+                                    account_id=Stack.of(self).account),
+                                managed_policies=[iam.ManagedPolicy.from_aws_managed_policy_name(
+                                    'AWSSupportAccess')],
+                                role_name='AWSSupportAccess',
+                                )
+
+        account_password_policy_parameters = {
+            "AllowUsersToChangePassword": True,
+            "HardExpiry": False,
+            "MaxPasswordAge": 90,
+            "MinimumPasswordLength": 14,
+            "PasswordReusePrevention": 24,
+            "RequireLowercaseCharacters": True,
+            "RequireNumbers": True,
+            "RequireSymbols": True,
+            "RequireUppercaseCharacters": True,
+        }
+
+        passwordPolicy = cr.AwsCustomResource(self, "PasswordPolicy",
+                                              on_create=cr.AwsSdkCall(
+                                                  service="IAM",
+                                                  action="updateAccountPasswordPolicy",
+                                                  parameters=account_password_policy_parameters,
+                                                  physical_resource_id=cr.PhysicalResourceId.of(
+                                                      "AccountPasswordPolicy"),
+                                              ),
+                                              on_update=cr.AwsSdkCall(
+                                                  service="IAM",
+                                                  action="updateAccountPasswordPolicy",
+                                                  parameters=account_password_policy_parameters,
+                                                  physical_resource_id=cr.PhysicalResourceId.of(
+                                                      "AccountPasswordPolicy"),
+                                              ),
+                                              on_delete=cr.AwsSdkCall(
+                                                  service="IAM",
+                                                  action="deleteAccountPasswordPolicy",
+                                              ),
+                                              policy=cr.AwsCustomResourcePolicy.from_sdk_calls(
+                                                  resources=cr.AwsCustomResourcePolicy.ANY_RESOURCE
+                                              )
+                                              )
+
+        default_ebs_encryption = cr.AwsCustomResource(self, "DefaultEbsEncryption",
+                                                      on_create=cr.AwsSdkCall(
+                                                            service="EC2",
+                                                            action="enableEbsEncryptionByDefault",
+                                                            physical_resource_id=cr.PhysicalResourceId.of(
+                                                                "EbsEncryptionByDefault"),
+                                                      ),
+                                                      on_update=cr.AwsSdkCall(
+                                                          service="EC2",
+                                                          action="enableEbsEncryptionByDefault",
+                                                          physical_resource_id=cr.PhysicalResourceId.of(
+                                                              "EbsEncryptionByDefault"),
+                                                      ),
+                                                      on_delete=cr.AwsSdkCall(
+                                                          service="EC2",
+                                                          action="disableEbsEncryptionByDefault",
+                                                      ),
+                                                      policy=cr.AwsCustomResourcePolicy.from_sdk_calls(
+                                                          resources=cr.AwsCustomResourcePolicy.ANY_RESOURCE
+                                                      )
+                                                      )
+
+        put_public_access_block = cr.AwsCustomResource(self, "putPublicAccessBlock",
+                                                       on_create=cr.AwsSdkCall(
+                                                           service="S3Control",
+                                                           action="putPublicAccessBlock",
+                                                           physical_resource_id=cr.PhysicalResourceId.of(
+                                                               "putPublicAccessBlockid"),
+                                                           parameters={
+                                                               "AccountId": Stack.of(self).account,
+                                                               "PublicAccessBlockConfiguration": {
+                                                                   "BlockPublicAcls": True,
+                                                                   "BlockPublicPolicy": True,
+                                                                   "IgnorePublicAcls": True,
+                                                                   "RestrictPublicBuckets": True,
+                                                               }
+                                                           }
+                                                       ),
+                                                       #   on_update=cr.AwsSdkCall(
+                                                       #   on_delete=cr.AwsSdkCall(
+                                                       policy=cr.AwsCustomResourcePolicy.from_statements(
+                                                           [iam.PolicyStatement(
+                                                               actions=[
+                                                                   "s3control:PutPublicAccessBlock", "s3:PutAccountPublicAccessBlock"],
+                                                               effect=iam.Effect.ALLOW,
+                                                               resources=['*']
+                                                           )]
+                                                       )
+                                                       )
+
+        guardduty_detector = guardduty.CfnDetector(self, 'GuardDutyDetector',
+                                                   enable=True
+                                                   )
